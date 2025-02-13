b161de9d924477b1a598abc763846b9ee597cd2e
s0ph0s <awoo@s0ph0s.dog>
2025-02-13 01:37:04 -0500

    flake: reformat with Alejandra

--- flake.nix
+++ flake.nix
@@ -10,41 +10,43 @@
     };
   };
 
-  outputs = { self, cosmo, nixpkgs }:
-    let
+  outputs = {
+    self,
+    cosmo,
+    nixpkgs,
+  }: let
+    # to work with older version of flakes
+    lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
 
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+    # Generate a user-friendly version number.
+    version = "1.0.0";
 
-      # Generate a user-friendly version number.
-      version = "1.0.0";
+    # System types to support.
+    supportedSystems = ["x86_64-linux" "x86_64-darwin"]; #"aarch64-linux" "aarch64-darwin" ];
 
-      # System types to support.
-      supportedSystems = [ "x86_64-linux" "x86_64-darwin" ]; #"aarch64-linux" "aarch64-darwin" ];
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay ]; });
-
-    in
-
-    {
-
-      # A Nixpkgs overlay.
-      overlay = final: prev: {
-
-        bskyfeed = with final; stdenv.mkDerivation rec {
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [self.overlay];
+      });
+  in {
+    formatter.x86_64-darwin = nixpkgs.legacyPackages.x86_64-darwin.alejandra;
+    # A Nixpkgs overlay.
+    overlay = final: prev: {
+      bskyfeed = final.stdenv.mkDerivation rec {
           pname = "bskyfeed";
           inherit version;
 
           src = ./.;
 
           nativeBuildInputs = [
-            cosmo.defaultPackage.${pkgs.stdenv.hostPlatform.system}
-            zip
-            gnumake
+            cosmo.defaultPackage.${final.pkgs.stdenv.hostPlatform.system}
+            final.zip
+            final.gnumake
           ];
 
           dontCheck = true;
@@ -55,7 +57,7 @@
           buildPhase = ''
             runHook preBuild
 
-            cp "${cosmo.defaultPackage.${pkgs.stdenv.hostPlatform.system}}/bin/redbean" ./redbean-3.0beta.com
+            cp "${cosmo.defaultPackage.${final.pkgs.stdenv.hostPlatform.system}}/bin/redbean" ./redbean-3.0beta.com
             ls .
             make build
 
@@ -71,41 +73,37 @@
             runHook postInstall
           '';
         };
-
-      };
-
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (system:
-        {
-          inherit (nixpkgsFor.${system}) bskyfeed;
-        });
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.bskyfeed);
-
-      # A NixOS module, if applicable (e.g. if the package provides a system service).
-      nixosModules.bskyfeed =
-        { pkgs, ... }:
-        {
-          nixpkgs.overlays = [ self.overlay ];
-
-          users.groups.bskyfeed = {};
-          users.users.bskyfeed = {
-            isSystemUser = true;
-            group = "bskyfeed";
-          };
-          systemd.services.bskyfeed = {
-            path = [ pkgs.bskyfeed ];
-            script = "bskyfeed.com -l 127.0.0.1";
-            wantedBy = [ "multi-user.target" ];
-            serviceConfig = {
-              Type = "simple";
-              User = "bskyfeed";
-              WorkingDirectory = "/tmp/bskyfeed";
-            };
-          };
-        };
     };
+
+    # Provide some binary packages for selected system types.
+    packages = forAllSystems (system: {
+      inherit (nixpkgsFor.${system}) bskyfeed;
+    });
+
+    # The default package for 'nix build'. This makes sense if the
+    # flake provides only one package or there is a clear "main"
+    # package.
+    defaultPackage = forAllSystems (system: self.packages.${system}.bskyfeed);
+
+    # A NixOS module, if applicable (e.g. if the package provides a system service).
+    nixosModules.bskyfeed = {pkgs, ...}: {
+      nixpkgs.overlays = [self.overlay];
+
+      users.groups.bskyfeed = {};
+      users.users.bskyfeed = {
+        isSystemUser = true;
+        group = "bskyfeed";
+      };
+      systemd.services.bskyfeed = {
+        path = [pkgs.bskyfeed];
+        script = "bskyfeed.com -l 127.0.0.1";
+        wantedBy = ["multi-user.target"];
+        serviceConfig = {
+          Type = "simple";
+          User = "bskyfeed";
+          WorkingDirectory = "/tmp/bskyfeed";
+        };
+      };
+    };
+  };
 }
